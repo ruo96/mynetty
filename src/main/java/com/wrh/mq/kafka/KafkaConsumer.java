339045654d7fade4e3beffaed5010b771bde48ef
wuruohong <wuruohong@bilibili.com>
2020-03-05 16:58:43 +0800

    add kafka document

--- src/main/java/com/wrh/mq/kafka/KafkaConsumer.java
+++ src/main/java/com/wrh/mq/kafka/KafkaConsumer.java
@@ -15,8 +15,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(id = "group123",topics = "mynetty123")
+    /**
+     * .\kafka-consumer-groups.bat --new-consumer --bootstrap-server 127.0.0.1:9092 --list       .\kafka-consumer-groups.bat --zookeeper 127.0.0.1:2181 --list  这个地方测试是有问题的
+     * .\kafka-topics.bat --zookeeper 127.0.0.1:2181 --topic hellonetty4 --describe    查看topic的详情
+     * .\kafka-server-start.bat D:\tools\mq\kafka\kafka_2.12-2.4.0\config\server.properties
+     * .\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic mynetty3-3p
+     * .\kafka-console-producer.bat --broker-list localhost:9092 --topic mynetty3-3p
+     * 如果有3个partition，那么就会ABA-ABA-ABA 这样收发数据，如果2个partition，那么AB就会轮流获取，如果只有一个partition，那么就只有A或者只有B能够接收数据
+     * @param record
+     */
+    @KafkaListener(groupId = "group1",topics = "mynetty3-3p")
     public void processMessage(ConsumerRecord<Integer, String> record) {
-      log.info("===> kafka 接收信息： {}",record.value());
+        log.info("===> A kafka key: {} topic : {} 接收信息： {}",record.key(), record.topic(), record.value());
+    }
+
+    @KafkaListener(groupId = "group1",topics = "mynetty3-3p")
+    public void processMessage1(ConsumerRecord<Integer, String> record) {
+        log.info("===> B kafka key: {} topic : {} 接收信息： {}",record.key(), record.topic(), record.value());
+    }
+
+    @KafkaListener(groupId = "group2",topics = "mynetty2")
+    public void processMessage2(ConsumerRecord<Integer, String> record) {
+        log.info("===> C kafka topic : {} 接收信息： {}",record.topic(), record.value());
+    }
+
+    @KafkaListener(groupId = "group3",topics = "hellonetty4")
+    public void processMessage4(ConsumerRecord<Integer, String> record) {
+        log.info("===> D kafka topic : {} 接收信息： {}",record.topic(), record.value());
     }
 }
--- src/main/java/com/wrh/mq/kafka/KafkaProducer.java
+++ src/main/java/com/wrh/mq/kafka/KafkaProducer.java
@@ -1,7 +1,17 @@
 package com.wrh.mq.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Created by wrh
@@ -13,11 +23,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaProducer {
 
-    /*@Autowired
-    private KafkaTemplate<Object,Object> template;
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
 
-    @GetMapping("/send/{input}")
-    public void sendFoo(@PathVariable String input){
-        template.send("topic", input);
-    }*/
+    @GetMapping("/topic/{topic}")
+    public void createTopic(@PathVariable("topic") String topic){
+
+        log.info("kafka createTopic start");
+        kafkaTemplate.setDefaultTopic(topic);
+        log.info("kafka createTopic end");
+    }
+
+
+    @GetMapping("/send/{topic}/{input}")
+    public void sendFoo(@PathVariable("topic") String topic,
+                        @PathVariable("input") String input){
+
+        log.info("kafka sendMessage start");
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, input);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("kafka sendMessage error, ex = {}, topic = {}, data = {}", ex, topic, input);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("kafka sendMessage success topic = {}, data = {}",topic, input);
+            }
+        });
+        log.info("kafka sendMessage end");
+    }
 }
