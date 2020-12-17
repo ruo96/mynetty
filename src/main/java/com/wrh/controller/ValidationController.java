90cee021640cf081d688b72718cfa3abe5b2d1cd
wuruohong <wuruohong@bilibili.com>
2020-12-17 16:41:19 +0800

    add validation

---
+++ src/main/java/com/wrh/controller/ValidationController.java
@@ -0,0 +1,47 @@
+package com.wrh.controller;
+
+import com.wrh.annotate.annotation.Student;
+import com.wrh.domain.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wuruohong
+ * @Classname ValidationController
+ * @Description TODO
+ * @Date 2020/12/17 16:21
+ */
+@Slf4j
+@RestController
+@Api(tags="入参校验 接口")
+public class ValidationController {
+
+    @ApiOperation(value = "插入用户信息", notes = "插入")
+    @PostMapping("/validate")
+    public List<String> swagger2(@Validated @RequestBody User user, BindingResult result){
+
+        List<String> errors = new ArrayList<>();
+        if(result.hasErrors()){
+            List<ObjectError> allErrors = result.getAllErrors();
+            allErrors.forEach(e->{
+                errors.add(e.getDefaultMessage());
+            });
+        }
+        log.info(">>> validate: {}", errors);
+        return errors;
+    }
+}
---
+++ src/main/java/com/wrh/domain/User.java
@@ -0,0 +1,30 @@
+package com.wrh.domain;
+
+import lombok.Data;
+
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author wuruohong
+ * @Classname User
+ * @Description TODO
+ * @Date 2020/12/17 16:18
+ */
+@Data
+public class User {
+    private Integer id;
+
+    @Size(min = 5,max = 10,message = "{user.name.size}")
+    private String name;
+
+    @NotNull(message = "{user.address.notnull}")
+    private String address;
+
+    @DecimalMin(value = "1",message = "{user.age.size}")
+    @DecimalMax(value = "200",message = "{user.age.size}")
+    private Integer age;
+
+}
